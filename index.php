c2f68e72690741c6402fbb294da4a01b7e59d022
zohreh-z <zahedi.zohreh@gmail.com>
2012-11-11 19:10:29 +0330

    add file index.php

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8" />
+	<title>EKBATAN</title>
+	<link href="1styles.css" rel="stylesheet" type="text/css" />
+	 <link href="favicon.ico" rel="shortcut icon" type="image/x-icon" />
+	 <link href="style.less" rel="stylesheet/less" type="text/css" />
+	 <script type="text/javascript" src="scripts/less.js">
+	 </script>
+	 <script type="text/javascript" src="scripts/zepto.js">
+	 </script>
+	 <script type="text/javascript" src="scripts/script.js">
+	 </script>
+
+</head>
+<body>
+	<section class="main">
+		<div class="main-txt">
+			<h1>hello! i'm shihab
+				<img src="images/shihabs_registeredlogo_tm.png">
+			</h1>
+			<h2>a Freelance web/graphic Designer.</h2>
+			<h3>Thank you for discovering me. This website is the virtual home of mine. I always had the passion to be creative in some way and  wanted to pursue it.
+        I figured, it was about time to see what i could do and find which avenue to take to be successful.</h3>
+        	<div class="buttonekbatan">what i do</div>
+        	<div class="buttonkanon">heir me</div>
+		</div>
+	</section>
+	<section id="button">
+		<ul>
+			<li>
+				<div class="header-bg">
+					<div class="header-txt cover">
+						<div class="header-icon">
+							<img src="images/icon_digitalart.png" alt="title" width="30" height="30" />
+						</div>
+						<div class="header-title">tite</div>
+					</div>
+				</div>
+				<div class="content">
+					<div class="txt">hdghfjabdjababdbdbadbabdbdabmdbabdbdabdbhcbhbhbdh</div>
+				</div>
+			</li>
+			<li>
+				<div class="header-bg">
+					<div class="header-txt cover">
+						<div class="header-icon">
+							<img src="images/icon_digitalart.png" alt="title" width="30" height="30" />
+						</div>
+						<div class="header-title">tite2</div>
+					</div>
+				</div>
+				<div class="content">
+					<div class="txt">hdghfjabdjababdbdbadbabdbdabmdbabdbdabdbhcbhbhbdh</div>
+				</div>
+			</li>
+			<li>
+				<div class="header-bg">
+					<div class="header-txt cover">
+						<div class="header-icon">
+							<img src="images/icon_digitalart.png" alt="title" width="30" height="30" />
+						</div>
+						<div class="header-title">tite3</div>
+					</div>
+				</div>
+				<div class="content">
+					<div class="txt">hdghfjabdjababdbdbadbabdbdabmdbabdbdabdbhcbhbhbdh</div>
+				</div>
+			</li>
+			<li>
+				<div class="header-bg">
+					<div class="header-txt cover">
+						<div class="header-icon">
+							<img src="images/icon_digitalart.png" alt="title" width="30" height="30" />
+						</div>
+						<div class="header-title">tite4</div>
+					</div>
+				</div>
+				<div class="content">
+					<div class="txt">hdghfjabdjababdbdbadbabdbdabmdbabdbdabdbhcbhbhbdh</div>
+				</div>
+			</li>
+		</ul>
+	</section>
+</body>
+</html>
