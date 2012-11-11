c162e0fe89022e67bc2199462d136ba458bce9f9
zohreh-z <zahedi.zohreh@gmail.com>
2012-11-11 19:56:49 +0330

    open & close menu ok but not complete

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,37 @@
 $(function(){
 	var ListMenu = $('section#button ul li'),
 		ContentList = $('section#button ul li div.content'),
-		flag = false;
-
-	ListMenu.click(function(){
-		indexList = ($(this).index());
-		if (!flag) {
-			ContentList.eq(indexList).animate({
+		flag = false,
+		current = -1;
+	function OpenMenu(){
+		ContentList.eq(indexList).animate({
 				height: 100,
 				'display': 'block',
 			},300);
-			flag = true;
+		flag = true;
+		current = indexList;
+	}; 
+	ListMenu.click(function(){
+		indexList = ($(this).index());
+		if (current != -1) {
+			ContentList.eq(current).animate({
+				height: 0,
+				'display' : 'none',
+			},300);
+			flag = false;
+			if (current == indexList) {
+				flag = true;
+			};
+		};
+		if (!flag) {
+			OpenMenu();	
 		}else{
 			ContentList.eq(indexList).animate({
 				height: 0,
 				'display': 'none',
 			},300);
-			flag = false;
-		}
+		 	flag = false;
+		 }
 	});
 	
 });
