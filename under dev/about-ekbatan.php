7c20f19dc7feff03ab73f5f201e4abc92edf2ea5
zohreh-z <zahedi.zohreh@gmail.com>
2012-11-13 23:34:41 +0330

    add file about-ekbatan.php

---
+++ under dev/about-ekbatan.php
@@ -0,0 +1,43 @@
+<div class="about-ekbatan">
+	<table>
+		<caption>Contact Information</caption>
+		<tr>
+			<th><p class="left">mahla delpak</p></th>
+			<th></th>
+			<th><p class="right">zohre zahedy</p></th>
+		</tr>
+		<tr>
+			<td>
+				<a class="left" href="https://github.com/mahlad" title="My github">https://github.com/mahlad</a>
+			</td>
+			<td class="icon">
+				<img class="git" src="images/phone_handset.png" alt="My github">
+			</td>
+			<td>
+				<a class="right" href="https://github.com/zohreh-z" title="My github">https://github.com/zohreh-z</a>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<a class="left" href="http://mahlad.ir" title="My website">http://mahlad.ir/</a>
+			</td>
+			<td class="icon">
+				<img  src="images/www.png" alt="My website">
+			</td>
+			<td >
+				<a class="right" href="http://www.zahedy.ir" title="My website">http://www.zahedy.ir</a>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<p class="left">mahlad365@gmail.com</p>
+			</td>
+			<td class="icon">
+				<img class="git" src="images/gmail.png" alt="My github">
+			</td>
+			<td>
+				<p class="right">zahedi.zohreh@gmail.com</p>
+			</td>
+		</tr>
+	</table>
+</div>
