167aa0d3098ebef7f1870c6b19d6f72e007ba171
zohreh-z <zahedi.zohreh@gmail.com>
2012-11-14 11:46:24 +0330

    splite file index.php

---
+++ inde2x.php
@@ -0,0 +1,120 @@
+<?php 
+	include "./inc/header.php";
+?>
+	<section class="main">
+		<div class="main-txt">
+			<h1>hello! i'm shihab
+				<img src="images/shihabs_registeredlogo_tm.png">
+			</h1>
+			<h2>a Freelance web/graphic Designer.</h2>
+			<h3>Thank you for discovering me. This website is the virtual home of mine. I always had the passion to be creative in some way and  wanted to pursue it.
+        I figured, it was about time to see what i could do and find which avenue to take to be successful.</h3>
+        	<div class="buttonekbatan">شرکت</div>
+        	<div class="buttonkanon">کانون</div>
+		</div>
+	</section>
+	<section id="button">
+		<ul>
+			<li>
+				<div class="header-bg">
+					<div class="header-txt cover">
+						<div class="header-icon">
+							<img src="images/icon_digitalart.png" alt="title" width="30" height="30" />
+						</div>
+						<div class="header-title">تبلیغات</div>
+					</div>
+				</div>
+				<div class="content">
+					<div class="txt">hdghfjabdjababdbdbadbabdbdabmdbabdbdabdbhcbhbhbdh</div>
+				</div>
+			</li>
+			<li>
+				<div class="header-bg">
+					<div class="header-txt cover">
+						<div class="header-icon">
+							<img src="images/icon_digitalart.png" alt="title" width="30" height="30" />
+						</div>
+						<div class="header-title">مقاله ها</div>
+					</div>
+				</div>
+				<div class="content">
+					<div class="txt">
+						<?php 
+							include "article.php";
+						?>
+					</div>
+				</div>
+			</li>
+			<li>
+				<div class="header-bg">
+					<div class="header-txt cover">
+						<div class="header-icon">
+							<img src="images/icon_digitalart.png" alt="title" width="30" height="30" />
+						</div>
+						<div class="header-title">گالری</div>
+					</div>
+				</div>
+				<div class="content">
+					<div class="txt">hdghfjabdjababdbdbadbabdbdabmdbabdbdabdbhcbhbhbdh</div>
+				</div>
+			</li>
+			<li>
+				<div class="header-bg">
+					<div class="header-txt cover">
+						<div class="header-icon">
+							<img src="images/icon_digitalart.png" alt="title" width="30" height="30" />
+						</div>
+						<div class="header-title">خدمات</div>
+					</div>
+				</div>
+				<div class="content">
+					<div class="txt">hdghfjabdjababdbdbadbabdbdabmdbabdbdabdbhcbhbhbdh</div>
+				</div>
+			</li>
+			<li>
+				<div class="header-bg">
+					<div class="header-txt cover">
+						<div class="header-icon">
+							<img src="images/icon_digitalart.png" alt="title" width="30" height="30" />
+						</div>
+						<div class="header-title">ایده ها</div>
+					</div>
+				</div>
+				<div class="content">
+					<div class="txt">سلام سلام سلام سلام سلام سلام سلام سلام سلام سلام سلام سلام سلام سلام سلام سلام سلام سلام سلام سلام سلام سلام سلام سلام سلام سلام سلام </div>
+				</div>
+			</li>
+		</ul>
+	</section>
+	<div class="line"></div>
+	<section id="end">
+		<div class="text-left">
+			<h1 class="contactus">what ....</h1>
+			<div class="txt">
+				<div class="content-txt">
+					reatively, Shihab has fantastic talent and elegant style. After Shihab did such a fantastic job redesigning our corporate logo there was no question we hoped to recruit him to update our website. His
+				</div>
+			</div>
+			<div class="btn-read">
+				<a href="#">
+					<img src="images/button_readall.png" />
+				</a>
+			</div>
+			
+		</div>
+		<div class="text-right">
+			<h1 class="contactus">Contact us ....</h1>
+			<div class="contact">
+				The best way to reach me is through email. Please feel free to contact me anytime for any work related queries. You're always welcome :)
+				<form>
+					<input type="text" class="name" id="name" />
+					<input type="text" class="email" id="email" />
+					<textarea class="textarea" id="texterea" placeholder="Your queries"></textarea>
+					<input type="image" name="btnimg" id="btnimg" src="images/button_sendme.png" />
+				</form>
+			</div>
+		</div>
+	</section>
+<?php 
+	include "./inc/footer.php";
+?>
