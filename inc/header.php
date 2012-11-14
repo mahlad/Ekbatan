03608664df4fc5032cfff81f6fe2f566d57a634a
zohreh-z <zahedi.zohreh@gmail.com>
2012-11-14 11:46:46 +0330

    create file header.php

---
+++ inc/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8" />
+	<title>EKBATAN</title>
+	<link href="1styles.css" rel="stylesheet" type="text/css" />
+	 <link href="favicon.ico" rel="shortcut icon" type="image/x-icon" />
+	 <link href="style.less" rel="stylesheet/less" type="text/css" />
+	 <script type="text/javascript" src="scripts/less.js">
+	 </script>
+	 <script type="text/javascript" src="scripts/zepto.js">
+	 </script>
+	 <script type="text/javascript" src="scripts/script.js">
+	 </script>
+
+</head>
+<body>
