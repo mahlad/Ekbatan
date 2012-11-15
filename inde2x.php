9cd314d4d68dc426da3fbd409f26dd44865b1ce5
zohreh-z <zahedi.zohreh@gmail.com>
2012-11-15 11:52:29 +0330

    add div fro shadow end

--- inde2x.php
+++ inde2x.php
@@ -102,34 +102,36 @@
 	</section>
 	<div class="line"></div>
 	<section id="end">
-		<div class="text-left">
-			<h1 class="contactus">آنچه را که آنها می خوانم</h1>
-			<div class="txt">
-				<div class="content-txt">
-					خلاقانه، شهاب دارای استعداد فوق العاده و به سبک ظریف است. پس از شهاب چنین یک کار فوق العاده در طراحی مجدد آرم شرکت ما بود شکی نیست ما امیدوار بودیم که تا او را استخدام برای به روز رسانی وب سایت ما وجود دارد. اشتیاق او به اجازه جریان خلاقیت در درون ما دستورالعمل و نه سخت او را مجموعه جدا "
+		<div class="content-end">
+			<div class="text-left">
+				<h1 class="contactus">آنچه را که آنها می خوانم</h1>
+				<div class="txt">
+					<div class="content-txt">
+						خلاقانه، شهاب دارای استعداد فوق العاده و به سبک ظریف است. پس از شهاب چنین یک کار فوق العاده در طراحی مجدد آرم شرکت ما بود شکی نیست ما امیدوار بودیم که تا او را استخدام برای به روز رسانی وب سایت ما وجود دارد. اشتیاق او به اجازه جریان خلاقیت در درون ما دستورالعمل و نه سخت او را مجموعه جدا "
 
-- تراویس Dugan
-مدیر ارتباطات، MuniSoft
+	- تراویس Dugan
+	مدیر ارتباطات، MuniSoft
 
+					</div>
 				</div>
+				<div class="btn-read">
+					<a href="#">
+						<img src="images/button_readall.png" />
+					</a>
+				</div>
+				
 			</div>
-			<div class="btn-read">
-				<a href="#">
-					<img src="images/button_readall.png" />
-				</a>
-			</div>
-			
-		</div>
-		<div class="text-right txr">
-			<h1 class="contactus">ارتباط با ما...</h1>
-			<div class="contact">
-				بهترین راه برای رسیدن به من از طریق ایمیل است. لطفا در صورت تمایل با من تماس بگیرید در هر زمان برای نمایش داده های مربوط به کار هر. شما همیشه استقبال :)
-				<form>
-					<input type="text" class="name" id="name" />
-					<input type="text" class="email" id="email" />
-					<textarea class="textarea" id="texterea" placeholder="Your queries"></textarea>
-					<input type="image" name="btnimg" id="btnimg" src="images/button_sendme.png" />
-				</form>
+			<div class="text-right txr">
+				<h1 class="contactus">ارتباط با ما...</h1>
+				<div class="contact">
+					بهترین راه برای رسیدن به من از طریق ایمیل است. لطفا در صورت تمایل با من تماس بگیرید در هر زمان برای نمایش داده های مربوط به کار هر. شما همیشه استقبال :)
+					<form>
+						<input type="text" class="name" id="name" />
+						<input type="text" class="email" id="email" />
+						<textarea class="textarea" id="texterea" placeholder="Your queries"></textarea>
+						<input type="image" name="btnimg" id="btnimg" src="images/button_sendme.png" />
+					</form>
+				</div>
 			</div>
 		</div>
 	</section>
