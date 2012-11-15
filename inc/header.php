63c32de2783ae06914a24f24506ede9cc31fb128
zohreh-z <zahedi.zohreh@gmail.com>
2012-11-15 13:45:16 +0330

    add link files js & css for slide top

--- inc/header.php
+++ inc/header.php
@@ -6,10 +6,13 @@
 	<link href="1styles.css" rel="stylesheet" type="text/css" />
 	<link href="favicon.ico" rel="shortcut icon" type="image/x-icon" />
 	<link href="style.less" rel="stylesheet/less" type="text/css" />
+	<link type="text/css"rel="stylesheet" href="style-projects.css"/>
 	<link rel="stylesheet" type="text/css" href="style.css">
+
 	<script type="text/javascript" src="scripts/zepto.js">
 	</script>
 	<script type="text/javascript" src="scripts/cueslider.js"></script>
+	<script type="text/javascript" src="scripts/slide-top.js"></script>
 	<script type="text/javascript" src="scripts/less.js">
 	</script>
 	<script type="text/javascript" src="scripts/script.js">
