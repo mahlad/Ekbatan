11d034b4c98b57b7f5dc46f93883530c92d802f9
zohreh-z <zahedi.zohreh@gmail.com>
2012-11-15 17:38:02 +0330

    add html for page content-article.php

--- content-article.php
+++ content-article.php
@@ -1,24 +1,37 @@
 <?php 
 	include "./inc/header.php";
 ?>
-	<section class="content-article">
-		<article>
-			<h1>نام مقاله</h1>
-			<p>
-				مقاله مقاله مقاله مقاله مقاله مقاله مقاله مقاله مقاله مقاله مقاله مقاله مقاله مقاله مقاله مقاله مقاله مقاله مقاله مقالهمقاله مقاله مقاله مقاله مقاله مقاله مقاله مقاله مقاله مقاله مقاله مقاله مقاله مقاله مقاله مقاله مقاله مقاله مقاله مقالهمقاله مقاله مقاله مقاله مقاله مقاله مقاله مقاله مقاله مقاله مقاله مقاله مقاله مقاله مقاله مقاله مقاله مقاله مقاله مقاله 
-			</p>
-			<a href="article1.zip">
-				<div class="bg-top-a">
-					دانلود مقاله
-				</div>
-			</a>
-			<a href="#">
-				<div class="bg-top-a">
-					ارسال نظر
-				</div>
-			</a>
-		</article>
-	</section>
+<section class="content-article cover">
+	<div class="title cover">	
+		<div class="title-article left"></div>
+		<div class="middle"></div>	
+		<div class="end"></div>
+		<div class="txt-article ">
+			نام مقاله
+		</div>
+	</div>
+	<a href="#">
+		<div class="ribbon">
+			<div class="ribbon-left"></div>	
+			<div class="tab"></div>
+			<div class="txt-article2 ">
+				دانلود فایل
+			</div>
+		</div>	
+	</a>
+	<a href="#">
+		<div class="ribbon2 cover">
+			<div class="ribbon-left2"></div>	
+			<div class="tab2"></div>
+			<div class="txt-article2 ">
+			ارسال نظر
+			</div>
+		</div>	
+	</a>
+	<div class="shadow">
+		<p>bla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkablakabla bkabla bkabla bkabla bkabla bkabla abla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla abla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla abla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla abla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla abla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla kabla bkabla bkabla bkabla bkabla bkabla bkabla abla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla abla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla abla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla abla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla kabla bkabla bkabla bkabla bkabla bkabla bkabla abla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla abla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla abla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla abla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla kabla bkabla bkabla bkabla bkabla bkabla bkabla abla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla abla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla abla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla abla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla abla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bkabla bka</p>	
+	</div>	
+</section>
 <?php 
 	include "./inc/footer.php";
 ?>
