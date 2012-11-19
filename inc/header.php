f8a35c0e9e158fcabb5932ae2233096577117cfa
zohreh-z <zahedi.zohreh@gmail.com>
2012-11-19 15:24:25 +0330

    delete comment

--- inc/header.php
+++ inc/header.php
@@ -8,15 +8,7 @@
 	<link href="style.less" rel="stylesheet/less" type="text/css" />
 	<link type="text/css"rel="stylesheet" href="style-slide-top.css"/>
 	<link rel="stylesheet" type="text/css" href="style.css">
-
-	<script type="text/javascript">
-	// var replaceDigits = function () {
-	// 	var map = ["&\#1776;","&\#1777;","&\#1778;","&\#1779;","&\#1780;", "&\#1781;","&\#1782;","&\#1783;","&\#1784;","&\#1785;"]
-	// 	document.body.innerHTML = document.body.innerHTML.replace(/\d(?=[^<>]*(<|$))/g, function($0) { return map[$0]});
-	// };
-	// 	window.onload = replaceDigits;
-	</script>
-
+	
 	<script type="text/javascript">
 		document.write('<script type="text/javascript" src=scripts/' + ('__proto__' in {} ? 'zepto' : 'jquery') + '.js><\/script>');
 	</script>
