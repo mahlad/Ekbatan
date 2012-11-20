209b504484a03ab3cac852f982ea52a5c4c3890a
zohreh-z <zahedi.zohreh@gmail.com>
2012-11-20 10:17:40 +0330

    create  special marketing.php

---
+++ special-marketing.php
@@ -0,0 +1,9 @@
+<?php 
+	include './inc/header.php';
+?>
+	<section class="fancy">
+		<article class="content-sp-market">
+			jdnvdfnvjk sjnjknkjsn scjksddjskn
+		</article>
+		<a id="fancy-close"></a>
+	</section>
